a5373ad3d2e662e3c0fcb35ae3fd0d833f4febf4
hogumachu <castle_jun@daum.net>
2023-02-24 18:12:11 +0900

    feat: Main에 Weather API Request 추가

--- OpenWeather/Main/MainViewModel.swift
+++ OpenWeather/Main/MainViewModel.swift
@@ -6,7 +6,36 @@
 //
 
 import Foundation
+import Moya
+import RxSwift
 
 final class MainViewModel {
+    
+    typealias RequestModel = Weather.ForecastRequestModel
+    typealias ResponseModel = Weather.ForecastResponseModel
+    
+    init(weatherProvider: MoyaProvider<WeatherAPI>) {
+        self.weatherProvider = weatherProvider
+    }
+    
+    private func requestWeather(lat: String, lon: String) {
+        let requestModel = RequestModel(lat: lat, lon: lon)
+        self.weatherProvider
+            .request(.forecast(requestModel: requestModel))
+            .subscribe(onSuccess: { [weak self] (model: ResponseModel?) in
+                self?.performAfterFetchingWeather(model)
+            }, onFailure: { error in
+                // TODO: - Handle Error
+                print(error.localizedDescription)
+            })
+            .disposed(by: self.disposeBag)
+    }
+    
+    private func performAfterFetchingWeather(_ model: ResponseModel?) {
+        
+    }
+    
+    private let weatherProvider: MoyaProvider<WeatherAPI>
+    private let disposeBag = DisposeBag()
     
 }
